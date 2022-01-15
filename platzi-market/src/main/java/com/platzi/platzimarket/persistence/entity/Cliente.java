45543cec0affca629f94e5b0ce92c9342b43fa94
publiruiz <publiruiz@outlook.com>
2022-01-15 20:28:21 +0000

    initial commit and entities

---
+++ platzi-market/src/main/java/com/platzi/platzimarket/persistence/entity/Cliente.java
@@ -0,0 +1,9 @@
+package com.platzi.platzimarket.persistence.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+}
---
+++ platzi-market/src/main/java/com/platzi/platzimarket/persistence/entity/Producto.java
@@ -0,0 +1,2 @@
+package com.platzi.platzimarket.persistence.entity;public class Producto {
+}
